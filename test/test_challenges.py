da39804d82b2fd31ecc38b649cebaa937ed5ef72
Evan Sultanik <evan@sultanik.com>
2021-12-02 09:31:36 -0500

    Allow inputs to be shared across parts

--- test/test_challenges.py
+++ test/test_challenges.py
@@ -16,9 +16,12 @@ class TestChallenges(TestCase):
             for part in challenge_type:
                 default_input = INPUTS_DIR / f"day{challenge.day}part{part}.txt"
                 if not default_input.exists():
-                    sys.stderr.write(f"Warning: No default input for challenge {challenge.name} part {part}; "
-                                     f"add one at {default_input!s}")
-                    continue
+                    # see if we have a generic input for the entire day:
+                    default_input = INPUTS_DIR / f"day{challenge.day}.txt"
+                    if not default_input.exists():
+                        sys.stderr.write(f"Warning: No default input for challenge {challenge.name} part {part}; "
+                                         f"add one at {default_input!s}")
+                        continue
                 output_path = OUTPUTS_DIR / f"day{challenge.day}part{part}.txt"
                 with open(output_path, "w") as f:
                     retval = challenge(default_input, f).run_part(part)
