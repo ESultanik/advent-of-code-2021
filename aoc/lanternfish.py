2f087cbe4b5e7a844374716794743a3a3ba81d16
Evan Sultanik <evan@sultanik.com>
2021-12-06 00:14:07 -0500

    Finished day 6 part 2

--- aoc/lanternfish.py
+++ aoc/lanternfish.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 
 from . import Challenge
@@ -19,3 +20,18 @@ class Lanternfish(Challenge):
                     new_ages.append(age - 1)
             ages = new_ages
         self.output.write(f"{len(ages)}")
+
+    @Challenge.register_part(1)
+    def many_days(self):
+        with open(self.input_path, "r") as f:
+            ages = Counter([int(age) for age in f.read().split(",")])
+        for i in range(256):
+            new_ages = Counter()
+            for age, num_fish in ages.items():
+                if age == 0:
+                    new_ages[6] += num_fish
+                    new_ages[8] += num_fish
+                else:
+                    new_ages[age - 1] += num_fish
+            ages = new_ages
+        self.output.write(f"{sum(ages.values())}")
